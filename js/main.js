d31011c207fde1d79abfa1a9bd11db507ef04ae7
Marvin-Erazo <Marvin-Erazo@hotmail.com>
2022-03-23 16:11:53 -0500

    desktop finished

--- js/main.js
+++ js/main.js
@@ -1,17 +1,54 @@
 
 function showAnswer(e){
 
-    let answer = e.target.parentNode.nextElementSibling;
+    let button = e.target;
+    console.log(button);
+
+    let answer = button.parentNode.nextElementSibling;
+    let paragraph = button.previousElementSibling;
+
+    switch (answer.className) {
+        case "disabled":
+            let activeElements = document.querySelectorAll('.active');
+
+            if(activeElements.length>0){
+                activeElements.forEach(element => {
+                    disableElements();
+                });
+            }
+
+            Active(answer,paragraph,button);
+            break;
     
-    console.log(answer);
-    
-    if (answer.className === "disabled"){
+        default:
+            Disabled(answer,paragraph,button);
+            break;
+    }
+
+}
+
+function Active(answer,paragraph,button){
         answer.className = "active";
-        e.target.style.transform = 'rotate(180deg)';
-        e.target.style.transition = 'all .5s'
-    }
-    else{
-        answer.className = "disabled";
-        e.target.style.transform = 'rotate(0deg)';
-    }
+        button.style.transform = 'rotate(180deg)';
+        button.style.transition = 'all .5s';
+        paragraph.style.fontWeight = "700";
+}
+
+function Disabled(answer,paragraph,button){
+    answer.className = "disabled";
+    button.style.transform = 'rotate(0deg)';
+    paragraph.style.fontWeight = "400";
+}
+
+function disableElements(){
+    let buttons = document.querySelectorAll("button");
+
+    buttons.forEach(element => {
+        let answer = element.parentNode.nextElementSibling;
+        let paragraph = element.previousElementSibling;
+
+        if(answer.className === "active"){
+            Disabled(answer,paragraph,element)
+        }
+    });
 }
