95574c2833d94132502f7f8d9e7bb09df797a2a7
Marvin-Erazo <Marvin-Erazo@hotmail.com>
2022-03-25 15:06:48 -0500

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
+# Frontend Mentor - FAQ accordion component
 
+This is a solution to the [FAQ accordion component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+
+### Screenshot
+
+Mobile
+
+
+
+
+Desktop
+
+
+
+### Links
+
+- Solution URL: https://github.com/Marvin-Erazo/profile-card-component
+- Live Site URL: https://github.com/Marvin-Erazo/faq-accordion-card-main
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- JavaScript
+- SASS
+### What I learned
+
+CSS grid layout for show the elements. Use html tags for the accesibility, Keyframes animations for elements
+GIT versions
+CSS clean code
+SASS imports and mixins
+
+### Continued development
+
+CSS animations for some elements
+DOOM manipluation and events in JS
+CSS Clean code
+HTML accesibility
+
+### Useful resources
+
+- [CSS Grid](https://developer.mozilla.org/es/docs/Web/CSS/CSS_Grid_Layout) - This helped me for set the grid rows and columns
+
+## Author
+
+- Website - [Marvin Erazo](https://marvin-erazo.github.io/)
+- Frontend Mentor - [@Marvin-Erazo](https://www.frontendmentor.io/profile/Marvin-Erazo)
+- GitHub - [Marvin-Erazo](https://github.com/Marvin-Erazo)
--- js/main.js
+++ js/main.js
@@ -1,32 +1,42 @@
 
+//This function catch the button element when this is clicked
 function showAnswer(e){
 
+    //the button clicked
     let button = e.target;
-    console.log(button);
-
+    //the paragrap of the faq answer this changes the class when the button is clicked
     let answer = button.parentNode.nextElementSibling;
+    //the faq title, this change the color when the faq answer is showed
     let paragraph = button.previousElementSibling;
 
+    //catch the class of the faq answer ot the button clicked
     switch (answer.className) {
+        //if this disabled the class change to active
+        //only show one FAQ per click
         case "disabled":
             let activeElements = document.querySelectorAll('.active');
 
+            //before close the other actives FAQs if there are any FAQ active
             if(activeElements.length>0){
                 activeElements.forEach(element => {
                     disableElements();
                 });
             }
 
+            //then change the class
             Active(answer,paragraph,button);
             break;
     
         default:
+            //if the FAQ answer is active the class change to disabled when click the button
             Disabled(answer,paragraph,button);
             break;
     }
 
 }
 
+//change the claas from disable to active
+//add a animation
 function Active(answer,paragraph,button){
         answer.className = "active";
         button.style.transform = 'rotate(180deg)';
@@ -34,12 +44,14 @@ function Active(answer,paragraph,button){
         paragraph.style.fontWeight = "700";
 }
 
+//change the class from active to disable
 function Disabled(answer,paragraph,button){
     answer.className = "disabled";
     button.style.transform = 'rotate(0deg)';
     paragraph.style.fontWeight = "400";
 }
 
+//change the class of the active FAQ answers
 function disableElements(){
     let buttons = document.querySelectorAll("button");
 
@@ -53,6 +65,8 @@ function disableElements(){
     });
 }
 
+
+//show/hide the attribution when the button is clicked
 function showAttribution(){
     let authorState = document.querySelector('#author');
     authorState.className = authorState.className === "author" ? "author-active" : "author"; 
